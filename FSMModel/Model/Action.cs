3bb5c69a8a5b54c0aac07ed55c72bfc2c4247fe7
Rafael Serrano <rsr.private@gmail.com>
2018-06-06 17:11:41 +0200

    Commit final del dia
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- FSMModel/Model/Action.cs
+++ FSMModel/Model/Action.cs
@@ -7,14 +7,28 @@
 
         private List<Command> commands = new List<Command>();
 
+        /// <summary>
+        /// Constructor de l'objecte.
+        /// </summary>
+        /// 
         public Action() {
         }
 
+        /// <summary>
+        /// Accepta un visitador.
+        /// </summary>
+        /// <param name="visitor">El visitador.</param>
+        /// 
         public void AcceptVisitor(IVisitor visitor) {
 
             visitor.Visit(this);
         }
 
+        /// <summary>
+        /// Afegeix una comanda.
+        /// </summary>
+        /// <param name="command">La comanda.</param>
+        /// 
         public void AddCommand(Command command) {
 
             if (command == null)
@@ -23,12 +37,20 @@
             commands.Add(command);
         }
 
+        /// <summary>
+        /// Enumera les comandes.
+        /// </summary>
+        /// 
         public IEnumerable<Command> Commands {
             get {
                 return commands;
             }
         }
 
+        /// <summary>
+        /// Indica si hi han comandes.
+        /// </summary>
+        /// 
         public bool HasCommands {
             get {
                 return commands.Count > 0;
