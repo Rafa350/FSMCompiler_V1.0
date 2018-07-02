e0f047858f0cae5950f738bbcadf199aa4e7e6c2
Rafael Serrano <rsr.private@gmail.com>
2018-07-02 11:30:38 +0200

    Refinaments en generador DOT
    Signed-off-by: Rafael Serrano <rsr.private@gmail.com>

--- FSMGenerator/Generator/DOT/DOTGenerator.cs
+++ FSMGenerator/Generator/DOT/DOTGenerator.cs
@@ -30,17 +30,33 @@
 
                 StringBuilder sb = new StringBuilder();
                 sb.AppendLine("digraph {");
-                sb.AppendLine("    ratio = \"fill\";");
-                sb.AppendLine("    size = \"8.3,11.7!\";");
-                sb.AppendLine("    margin = 0.5;");
+                sb.AppendLine("    ratio=\"fill\";");
+                sb.AppendLine("    size=\"8.3,11.7!\";");
+                sb.AppendLine("    margin=0.5;");
                 sb.AppendLine();
+
+                // Declara els estats
+                //
+                foreach (State state in machine.States) {
+                    sb.AppendFormat("    {0} [", state.Name);
+                    sb.AppendFormat("shape={0}", machine.Start == state ? "doublecircle" : "circle");
+                    sb.Append(", fontname=arial");
+                    sb.Append("];");
+                    sb.AppendLine();
+                }
+                sb.AppendLine();
+
+                // Declara les transicions
+                //
                 foreach (State state in machine.States) {
                     foreach (Transition transition in state.Transitions) {
                         sb.Append("    ");
                         sb.Append(state.Name);
-                        sb.Append(" -> ");
-                        sb.AppendFormat("{0}[", transition.NextState == null ? state.Name : transition.NextState.Name);
+                        sb.Append("->");
+                        sb.AppendFormat("{0} [", transition.NextState == null ? state.Name : transition.NextState.Name);
                         sb.AppendFormat("label={0}", transition.Event.Name);
+                        sb.Append(", fontname=arial");
+                        sb.Append(", fontsize=12");
                         sb.Append("];");
                         sb.AppendLine();
                     }
