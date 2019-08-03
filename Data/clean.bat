1b5271cb1501b7438f926ca5e07cb1e3a972f7a6
Rafael Serrano <rsr.openware@gmail.com>
2019-08-03 23:26:52 +0200

    Commit
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

--- Data/clean.bat
+++ Data/clean.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\Microchip\MPLABX\v4.20\gnuBins\GnuWin32\bin\make.exe" -f makefile.mak clean
+"C:\Program Files\Microchip\MPLABX\v5.10\gnuBins\GnuWin32\bin\make.exe" -f makefile.mak clean
--- Data/make.bat
+++ Data/make.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\Microchip\MPLABX\v4.20\gnuBins\GnuWin32\bin\make.exe" -f makefile.mak
+"C:\Program Files\Microchip\MPLABX\v5.10\gnuBins\GnuWin32\bin\make.exe" -f makefile.mak
 pause "Pulsar una tecla"
