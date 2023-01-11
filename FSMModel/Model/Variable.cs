14a0f55ef5ad17e5b192b175ebeedef9ed521740
Rafael Serrano <rsr.openware@gmail.com>
2023-01-11 21:17:21 +0100

    Commit
    Signed-off-by: Rafael Serrano <rsr.openware@gmail.com>

---
+++ FSMModel/Model/Variable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MikroPicDesigns.FSMCompiler.v1.Model {
+
+    public sealed class Variable: IVisitable {
+
+        private readonly string _name;
+        private readonly string _type;
+        private readonly string _value;
+
+        public Variable(string name, string type, string value = null) {
+
+            _name = name ?? throw new ArgumentNullException(nameof(name));
+            _type = type ?? throw new AccessViolationException(nameof(type));
+            _value = value;
+        }
+
+        public void AcceptVisitor(IVisitor visitor) {
+
+        }
+
+        public string Name =>
+            _name;
+
+        public string Type =>
+            _type;
+
+        public string Value =>
+            _value;
+    }
+}
